d1f6f76cdce30665c36255448254af61ea2f27bd
FelipeNevess <felipe012neves@gmail.com>
2021-06-18 18:33:38 -0300

    atividade 1 finalizado

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/parte2.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const c2 = (lesson, key, value) => {
+  return lesson[key] = value;
+}
+
+console.log(lesson2);
+console.log(c2(lesson2, 'turno', 'manhã'));
