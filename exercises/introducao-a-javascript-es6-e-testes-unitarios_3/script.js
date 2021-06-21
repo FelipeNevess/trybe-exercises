1f4ebadabebfc6113b6a1e6228c5db19a2ffaf3d
FelipeNevess <felipe012neves@gmail.com>
2021-06-21 19:41:23 -0300

    Restruturando a questao 1

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/script.js
@@ -12,8 +12,13 @@ function sum(a, b) {
 assert.strictEqual(sum(4, 5), 9);
 assert.strictEqual(sum(0, 0), 0);
 assert.strictEqual(typeof sum, 'function');
+
 assert.throws(() => {
   sum(4, '5')
 });
-assert.throws(sum(4, '5'), /^parameters must be numbers$/)
 
+assert.throws(() => {
+  sum(4, '5');
+}, /^Error: parameters must be numbers$/);
+
+////////////////////////////
