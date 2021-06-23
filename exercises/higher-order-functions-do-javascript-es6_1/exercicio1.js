d7adf54c8746c91cb438ff45b4b9230246dd4ee3
FelipeNevess <felipe012neves@gmail.com>
2021-06-23 18:18:47 -0300

    Atividade 1 finalizado

---
+++ exercises/higher-order-functions-do-javascript-es6_1/exercicio1.js
@@ -0,0 +1,18 @@
+const a = (nome) => {
+  b = {
+    nomeCompleto: `${nome}`,
+    email: nome.split(' ').join('') + '@trybe.com',
+  }
+  return b;
+};
+
+const newEmployees = () => {
+  const employees = {
+    id1: a('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: a('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: a('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(a))
