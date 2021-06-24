8d8ba433be8676b605f79e5604ce1a00f3def260
FelipeNevess <felipe012neves@gmail.com>
2021-06-24 08:55:54 -0300

    Atividade 3 finalizada

---
+++ exercises/higher-order-functions-do-javascript-es6_1/exercicio3.js
@@ -0,0 +1,23 @@
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const result = () => {
+  let cont = 0;
+  for (let i in rightAnswers) {
+    if (rightAnswers[i] === studentAnswers[i]) {
+      cont += 1;
+    } else if ('N.A' === studentAnswers[i]) {
+      cont += 0;
+    } else {
+      cont -= 0.5;
+    }
+  }
+  console.log(`O aluno acertou ${cont} de ${studentAnswers.length}`);
+  return cont;
+}
+
+const verificar = (an1, an2, callback) => {
+  return callback(an1, an2);
+}
+
+console.log(verificar(rightAnswers,studentAnswers,result));
