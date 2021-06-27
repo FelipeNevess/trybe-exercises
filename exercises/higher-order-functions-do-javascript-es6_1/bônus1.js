eb6eb43585e54f54b5a4586469ee1c4940c28bfd
FelipeNevess <felipe012neves@gmail.com>
2021-06-27 11:32:59 -0300

    Bonus impossivel dr fazer no momento

--- exercises/higher-order-functions-do-javascript-es6_1/bônus1.js
+++ exercises/higher-order-functions-do-javascript-es6_1/bônus1.js
@@ -20,7 +20,63 @@ const dragon = {
 
 const battleMembers = { mage, warrior, dragon };
 
-const dragonDano = () => Math.floor(Math.random() * (dragon.strength - 15)) + 15;
-const warriorDano = () => Math.floor(Math.random() * ((warrior.strength * warrior.weaponDmg) - warrior.strength)) + warrior.strength;
+const dragonAttack = (dragon) => {
+  const minDmg = 15;
+  const { strength } = dragon;
+  const dragonDmg = Math.floor((Math.random() * (strength - minDmg) + minDmg));
+  return dragonDmg;
+};
 
-console.log(warriorDano());
+const warriorAttack = (warrior) => {
+  const weaponDmg = warrior;
+  const minDmg = warrior.strength;
+  const maxDmg = minDmg * weaponDmg;
+  const warriorDamage = Math.floor((Math.random() * (maxDmg - minDmg)) + minDmg);
+  return warriorDamage;
+};
+
+const mageAttack = (mage) => {
+  const mageMana = mage.mana;
+  const minDmg = mage.intelligence;
+  const maxDmg = minDmg * 2;
+  const turnStats = {
+    manaSpent: 0,
+    damageDealt: 'Not enough mana...',
+  };
+
+  if (mageMana > 15) {
+    const mageDamage = Math.floor((Math.random() * (maxDmg - minDmg)) + minDmg);
+    turnStats.manaSpent = 15;
+    turnStats.damageDealt = mageDamage;
+    return turnStats;
+  }
+  return turnStats;
+};
+
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const warriorDamage = warriorAttack();
+    warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
+  },
+  mageTurn: (mageAttack) => {
+    const mageTurnStats = mageAttack();
+    const mageDamage = mageTurnStats.damageDealt;
+    const { manaSpent } = mageTurnStats;
+    mage.damage = mageDamage;
+    mage.mana -= manaSpent;
+    dragon.healthPoints -= mageDamage;
+  },
+  monsterTurn: (monsterAttack) => {
+    const dragonDamage = monsterAttack();
+    mage.healthPoints -= dragonDamage;
+    warrior.healthPoints -= dragonDamage;
+    dragon.damage = dragonDamage;
+  },
+  turnResults: () => battleMembers,
+};
+
+gameActions.warriorTurn(warriorAttack);
+gameActions.mageTurn(mageAttack);
+gameActions.monsterTurn(dragonAttack);
+console.log(gameActions.turnResults());
